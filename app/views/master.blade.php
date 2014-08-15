b908893d5337383a26385345a05c292d3f86422a
root <root@atomichael.com>
2014-08-15 11:41:45 -0400

    Added minor styling to master blade template. Also added main stylesheet

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -3,11 +3,27 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Demos</title>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <title>Atomichael</title>
+    {{ HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'); }}
+    {{ HTML::style('css/styles.css'); }}
+    {{ HTML::style('http://fonts.googleapis.com/css?family=Roboto'); }}
+	{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'); }}
+	<!--{{ HTML::script('//ajax.googleapis.com/ajax/libs/webfont/1.5.3/webfont.js'); }}-->
 </head>
 
 <body>
-<div class="container">@yield('container')</div>
+	<div id='header'>
+			<div='nav'>
+				<ul>
+					<li><a href='posts'>Blog</a></li>
+				</ul>
+			</div>
+	</div>
+	<div class="container">
+		@yield('container')
+	</div>
+	<div='footer'>
+
+	</div>
 </body>
 </html>
